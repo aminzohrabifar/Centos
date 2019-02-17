a9539a89773e349cdd892852ca94ddb57aed71b2
Amin Zohrabi Far <47514360+aminzohrabifar@users.noreply.github.com>
2019-02-17 21:50:24 +0330

    Create motd01.sh

---
+++ motd01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "
+ ----------------------------%Hello World%--------------------------------
+|                                                                         |
+| This Server Configure By Amin Zohrabi  And Auto Configuration Tool !!!! |
+|                                                                         |
+|                   Website : linkedin.com/in/aminzf                  |
+|                                                                         |
+| You can tell me for configure server and optimize : +98 916 80 30 360   |
+|                                                                         |
+| ------------------------------------------------------------------------
+" > /etc/motd
+chattr +i /etc/motd
